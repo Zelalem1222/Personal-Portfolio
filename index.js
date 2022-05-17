7fc765d6475d303cbfac654bd9d02440e52afddf
Zelalem1222 <zelalemfissha394@gmail.com>
2022-05-17 13:49:48 +0300

    make the cards on js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// const { object } = require("webidl-conversions");
+
+
 const nav = document.querySelector('nav');
 const main = document.createElement('div');
 main.className = 'mob-menu';
@@ -48,3 +51,87 @@ menu.addEventListener('click', () => {
     blur.classList.remove('active');
   });
 });
+
+// Work popup
+
+const works = [
+  {
+    id:1,
+    name : 'Tonic',
+    experiance : ['CANOPY' , 'Backend Dev' , '2015'],
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    languages : ['html' ,'Ruby on rails', 'css' , 'javaScript'],
+    popuplanguage : ['html' , 'css' , 'javaScript','github','ruby','bootstrap'],
+    popDescription : 'Dolor ipsam molestiae assumenda nisi natus voluptatum? Dolor ipsam molestiae assumenda nisi natus voluptatum? Dolor ipsam molestiae assumenda nisi natus voluptatum? Dolor ipsam molestiae assumenda nisi natus voluptatum?',
+    image : 'images/Snapshoot-Portfolio-desktop.png'  
+  },
+  {
+    id:2,
+    name : 'Multi-Post Stories',
+    experiance : ['FACEBOOK' , 'Backend Dev' , '2015'],
+    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    languages : ['html' ,'Ruby on rails', 'css' , 'javaScript'],
+    popuplanguage : ['html' , 'css' , 'javaScript','github','ruby','bootstrap'],
+    popDescription : 'Dolor ipsam molestiae assumenda nisi natus voluptatum? Dolor ipsam molestiae assumenda nisi natus voluptatum? Dolor ipsam molestiae assumenda nisi natus voluptatum? Dolor ipsam molestiae assumenda nisi natus voluptatum?',
+    image : 'images/Snapshoot-Portfolio-desktop-2.png' 
+  },
+  {
+    id:3,
+    name : 'Facebook 360',
+    experiance : ['Facebook' , 'Backend Dev' , '2015'],
+    description: 'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    languages : ['html' ,'Ruby on rails', 'css' , 'javaScript'],
+    popuplanguage : ['html' , 'css' , 'javaScript','github','ruby','bootstrap'],
+    popDescription : 'Dolor ipsam molestiae assumenda nisi natus voluptatum? Dolor ipsam molestiae assumenda nisi natus voluptatum? Dolor ipsam molestiae assumenda nisi natus voluptatum? Dolor ipsam molestiae assumenda nisi natus voluptatum?',
+    image : 'images/Snapshoot-Portfolio-desktop3.jpg' 
+  },
+  {
+    id:4,
+    name : 'Uber Navigation',
+    experiance : ['Uber' , 'Backend Dev' , '2015'],
+    description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    languages : ['html' ,'Ruby on rails', 'css' , 'javaScript'],
+    popuplanguage : ['html' , 'css' , 'javaScript','github','ruby','bootstrap'],
+    popDescription : 'Dolor ipsam molestiae assumenda nisi natus voluptatum? Dolor ipsam molestiae assumenda nisi natus voluptatum? Dolor ipsam molestiae assumenda nisi natus voluptatum? Dolor ipsam molestiae assumenda nisi natus voluptatum?',
+    image : 'images/Snapshoot-Porfolio-desktop4.png' 
+  }
+]
+
+for (let i=0; i < works.length; i++){
+  let technology = '';
+  let experianceList = '';
+  for(let j=0; j<works[i].languages.length; j++ ){
+    technology += `<li >${works[i].languages[j]}</li>`;
+  }
+  for(let k =0; k<works[i].experiance.length; k++){
+    experianceList += k===0 ? `<li><h3>${works[i].experiance[k]}</h3></li>` : `
+    <li class="counter"><img src="images/Counter.png" alt="Backend dev">${works[i].experiance[k]}</li>`;
+  } 
+ 
+
+ document.querySelector('.cards-desktop').innerHTML += `
+  <div class="card-d-1" id='work-${works[i].id}' >
+  <img class="snapshot-desktop${works[i].id}" src='${works[i].image}'>
+  <div class="description">
+   <h2>${works[i].name}</h2>  
+   <div>
+    <ul class="container">
+       ${experianceList}
+    </ul>
+   </div>
+   <p>${works[i].description}</p>
+   <div>
+        <ul class="languages">
+        ${technology}
+        </ul>
+    </div> 
+    
+ <button class="see more" type="button">See Project</button>
+ </div>
+   
+</div>
+ `
+}
+//  console.log(experianceList);
+// 
+// 
